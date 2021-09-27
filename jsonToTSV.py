294e4abab56fecb1b67a3ff00f88e60e05d21f62
James Truitt <jtruitt1@swarthmore.edu>
2021-09-27 09:30:02 -0400

    Create jsonToTSV.py

---
+++ jsonToTSV.py
@@ -0,0 +1,28 @@
+from csv import *
+from utils import loadSnacData
+
+def convertToTsv(constellations):
+	"""
+	Takes a list of SNAC constellation JSONs and returns a set of TSV rows
+	containing certain fields
+	"""
+	rows = []
+	# TODO: Add list of column headers
+	header = []
+	for constellation in constellations:
+
+
+def writeTsvToFile(rows):
+	pass
+
+def main():
+	# Import JSONs
+	constellations = loadSnacData()
+
+	# Create list of TSV rows from JSONs
+	rows = convertToTsv(constellations)
+
+	# Write rows to a file
+	writeTsvToFile(rows)
+
+main()
