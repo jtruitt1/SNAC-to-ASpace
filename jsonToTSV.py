34b87cccf9d6d19dd0d5ca52b7fd52a4426e3d50
James Truitt <jtruitt1@swarthmore.edu>
2021-09-27 09:33:30 -0400

    Update jsonToTSV.py

--- jsonToTSV.py
+++ jsonToTSV.py
@@ -1,6 +1,8 @@
 from csv import *
 from utils import loadSnacData
 
+# csv documentation: https://docs.python.org/3/library/csv.html#writer-objects
+
 def convertToTsv(constellations):
 	"""
 	Takes a list of SNAC constellation JSONs and returns a set of TSV rows
